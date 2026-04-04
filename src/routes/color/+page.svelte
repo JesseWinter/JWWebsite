362701b9f2de7b1d8229f7ec20b125f8645da11a
JesseWinter <jessewinter310@gmail.com>
2026-04-04 12:37:49 -0400

    cr change 2

--- src/routes/color/+page.svelte
+++ src/routes/color/+page.svelte
@@ -13,7 +13,7 @@
             <h2 class="reel-title"><strong>Color Grading Reel</strong></h2>
             <p class="reel-date">August 2025</p>
             <div class="reel-video">
-                <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1179944151?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="JESSEWINTER_COLORREEL_April2026"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+                <iframe src="https://player.vimeo.com/video/1179944151?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1920" height="1080" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin" title="JESSEWINTER_COLORREEL_April2026"></iframe>
             </div>
         </section>
     </main>
